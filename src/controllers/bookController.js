465b8a52ac357be6a373a402f58e559c971f088a
prince9871 <soniprince142@gmail.com>
2022-08-16 20:19:49 +0530

    BookModel Code

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,12 +3,15 @@ const BookModel= require("../models/bookModel")
 const createBook= async function (req, res) {
     let data= req.body
 
-    let savedData= await BookModel.create(data)
+    let savedData= await BookModel.create(data) //save
     res.send({msg: savedData})
 }
 
 const getBooksData= async function (req, res) {
     let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
+    //allBooks.authorName="PK"
+    //allBooks.save()
+    
     res.send({msg: allBooks})
 }
 
